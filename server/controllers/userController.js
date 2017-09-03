5ce89f87d51e70159bbe635a931b9095f5b4d8ec
nirmalks <kagosekar@gmail.com>
2017-09-03 13:52:55 +0530

    Added bcrypt for password hashing

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-
+var bcrypt = require('bcryptjs');
 let userController = {};
 
 userController.signup = (req,res,next) => {
     let userParams = req.body;
 
-	const user = new User({
-        username: userParams.username,
-        password: userParams.password,
-        email: userParams.email
+	const user = new User(); 
+    user.username =  userParams.username;
+    user.email =  userParams.email;
+ 
+    bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(userParams.password, salt, function(err, hash) {
+       user.password = hash;
+        user.save().then((user) => {
+            res.json({success:true, message: "Added successfully."});
+        } , (e) => {
+            res.json({success: false, message: `Failed to create a new list. Error: ${e}`});
+        });
     });
-    user.save().then((user) => {
-        res.json({success:true, message: "Added successfully."});
-    } , (e) => {
-        res.json({success: false, message: `Failed to create a new list. Error: ${e}`});
     });
+
+   
 };
 
 
@@ -24,17 +30,20 @@ userController.login = (req, res, next) => {
 
     let userParams = req.body;
     let email = userParams.email;
-    let password = userParams.password;
-    console.log(email);
-    console.log(password);
-    User.findOne({'email': email , 'password' : password } , function (err, user) {
-
+    
+    User.findOne({'email': email} , function (err, user) {
         if(err) {
               res.json({success: false, message: "Invalid username / password"}); 
         }
-        console.log(user);
         if(user != null) {
-            res.json({success: true, message: "Login successfull" , user: user });
+            bcrypt.compare(userParams.password, user.password).then((resp) => {
+                console.log(resp);
+                if(resp === true) {
+                    res.json({success: true, message: "Login successfull" , user: user });
+                } else {
+                    res.json({success: false, message: "Invalid username / password"}); 
+                }
+            });       
         } else {
             res.json({success: false, message: "Invalid username / password"}); 
         }
