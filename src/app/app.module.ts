e60a07f6a78d5994d2b1ab43f7fca66e6ff5466b
nirmalks <kagosekar@gmail.com>
2017-09-09 00:07:11 +0530

    1. Added login and signup basic layouts 2.Added routing 3.generated google auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
+import { NgSemanticModule } from 'ng-semantic/ng-semantic';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+import { appRoutes } from './app.routing.module';
+import { SignupComponent } from './signup/signup.component';
+import { GoogleAuthService } from './services/google-auth.service';
+import { baseUrl } from './shared/baseurl';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgSemanticModule,
+      RouterModule.forRoot(
+      appRoutes
+    )
   ],
-  providers: [],
+  providers: [GoogleAuthService,  {provide: 'BaseURL', useValue: baseURL}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
