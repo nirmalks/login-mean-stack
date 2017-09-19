63504fabece32c7b3da8c4fa32ec09e427df3efc
nirmalks <kagosekar@gmail.com>
2017-09-19 23:38:41 +0530

    Added signup form

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../shared/user';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  userDetails: User;
+  userForm: FormGroup;
+  
+  constructor(private fb: FormBuilder) { 
+    this.createForm();
+  }
 
   ngOnInit() {
   }
@@ -15,5 +21,19 @@ export class SignupComponent implements OnInit {
   googleSignup(){
     console.log("Google signup clicked");
     window.open('http://localhost:3000/google');
+  }
+
+   signup() {
+    console.log("login clicked");
+     this.userDetails = this.userForm.value;
+     console.log(this.userDetails);
+  }
+
+  createForm() {
+       this.userForm = this.fb.group({
+      username: ['', [Validators.required]  ],
+      email: ['', [Validators.required, Validators.email]  ],
+      password: ['', [Validators.required] ],
+    });
   }
 }
