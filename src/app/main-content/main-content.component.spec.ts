874326ec5c77b20d023f9ea0253e9f09d72a9a2e
Nirmal Kumar Sekar <kagosekar@gmail.com>
2017-12-28 22:51:42 +0530

    Added basic unit tests for DOM

--- src/app/main-content/main-content.component.spec.ts
+++ src/app/main-content/main-content.component.spec.ts
@@ -22,4 +22,16 @@ describe('MainContentComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render title in a h1 tag', async(() => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Sample MEAN Stack Application');
+  }));
+
+  it('should render subtitle in a h2 tag', async(() => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Featuring Angular 4 , Express , Mongoose and Passport Authentication.');
+  }));
 });
