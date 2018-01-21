c6043da5a99e60cec3da2d92c010aa165067e8af
Nirmal Kumar Sekar <kagosekar@gmail.com>
2018-01-21 20:54:36 +0530

    Removed unnecessary whitespace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
-
 import { HeaderComponent } from './header/header.component';
 import { RouterModule, Routes } from '@angular/router';
 import { appRoutes } from './app.routing.module';
